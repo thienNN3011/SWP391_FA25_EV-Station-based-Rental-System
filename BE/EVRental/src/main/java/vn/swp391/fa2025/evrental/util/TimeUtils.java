b7f209d2970335da736aae4347f53002a45b6ef9
Dai Duong <taaunn.003@gmail.com>
2025-11-22 22:03:33 +0700

    fix: Sửa lỗi 'Đơn vị không hợp lệ: DAILY' khi bắt đầu hợp đồng - Thêm hỗ trợ HOURLY, DAILY, WEEKLY, MONTHLY vào TimeUtils.ceilTimeDiff() - Fix lỗi khi tính toán thời gian thuê với tariff type từ database

--- BE/EVRental/src/main/java/vn/swp391/fa2025/evrental/util/TimeUtils.java
+++ BE/EVRental/src/main/java/vn/swp391/fa2025/evrental/util/TimeUtils.java
@@ -18,24 +18,28 @@ public class TimeUtils {
         switch (unit.toLowerCase()) {
             case "hour":
             case "hours":
+            case "hourly":
             case "giờ":
                 result = minutes / 60.0;
                 break;
 
             case "day":
             case "days":
+            case "daily":
             case "ngày":
                 result = minutes / (60.0 * 24);
                 break;
 
             case "week":
             case "weeks":
+            case "weekly":
             case "tuần":
                 result = minutes / (60.0 * 24 * 7);
                 break;
 
             case "month":
             case "months":
+            case "monthly":
             case "tháng":
                 result = minutes / (60.0 * 24 * 30);
                 break;
